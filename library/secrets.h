44d482434abdff068aef958135160cc3800fd7fa
NguyenDucLamK63 <lam.nd182626@gmail.com>
2021-05-13 22:44:05 +0700

    khoi_dau

---
+++ library/secrets.h
@@ -0,0 +1,8 @@
+// Use this file to store all of the private credentials 
+// and connection details
+
+#define SECRET_SSID "LinhLam"		// replace MySSID with your WiFi network name
+#define SECRET_PASS "23456789"	// replace MyPassword with your WiFi password
+
+#define SECRET_CH_ID 1382630		// replace 0000000 with your channel number
+#define SECRET_WRITE_APIKEY "1P4NI2Y8HIXBWE0I"   // replace XYZ with your channel write API Key
